f77b335b197281f7da91d4c93fc19bc9e4ee1dc3
AndreasS <github@selguson.se>
2018-04-05 10:47:58 +0200

    Added 2nd where expression for filtering out movies already returned.

--- 4.sql
+++ 4.sql
@@ -4,3 +4,4 @@ from leased t0
 left join movie t1 on t0.movie_id = t1.id
 left join customer t2 on t0.Customer_Id = t2.id
 where date_add(t0.until, interval 4 day) <= date(now())
+and returned is null
