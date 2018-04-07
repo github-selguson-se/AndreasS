707b609dc6c1642611337555af8c2935fa33c753
AndreasS <github@selguson.se>
2018-04-07 13:59:52 +0200

    Delete files used for develop, added Worddocument, recreated database.sql & model.mwb. Modified queries.sql

--- 4.sql
+++
@@ -1,7 +0,0 @@
-create view vwLateLease as
-select t1.title as Title, t2.name as CustomerName
-from leased t0
-left join movie t1 on t0.movie_id = t1.id
-left join customer t2 on t0.Customer_Id = t2.id
-where date_add(t0.StartDate, interval 4 day) <= now()
-and returndate is null
--- 7.sql
+++
@@ -1,5 +0,0 @@
-delimiter //
-create procedure spStartLease (IN movie_id INT, employee_id INT, customer_id INT)
-    begin
-		insert into leased (movie_id, employee_id, customer_id, startdate) values (movie_id, employee_id, customer_id, now());
-    end//
